2b0a07ee07d903c62393bc297f0b962bef81f8b4
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2024-02-06 00:33:40 +0700

    added parse function

--- src/main/java/cft/test/task/OptionsParser.java
+++ src/main/java/cft/test/task/OptionsParser.java
@@ -1,4 +1,57 @@
 package cft.test.task;
 
 public class OptionsParser {
+
+    static Options parse(String[] args) throws UserInputException {
+
+        Options.Builder builder = new Options.Builder();
+
+        for (int i = 0; i < args.length; ) {
+            switch (args[i]) {
+                case "-s" -> {
+                    builder.needShortStatistic();
+                    i += 1;
+                }
+
+                case "-a" -> {
+                    builder.notRewriteFiles();
+                    i += 1;
+                }
+
+                case "-f" -> {
+                    builder.needFullStatistic();
+                    i += 1;
+                }
+
+                case "-p" -> {
+
+                    if (i + 2 == args.length) {
+                        throw UserInputException.noArgument("-p");
+                    }
+
+                    builder.namesPrefix(args[i + 1]);
+                    ++i;
+                }
+
+                case "-o" -> {
+
+                    if (i + 2 == args.length) {
+                        throw UserInputException.noArgument("-o");
+                    }
+
+                    builder.resultPath(args[i + 1]);
+                    ++i;
+                }
+
+                default -> {
+
+                    //TODO input files
+                    ++i;
+                }
+            }
+        }
+
+        return builder.build();
+    }
+
 }
