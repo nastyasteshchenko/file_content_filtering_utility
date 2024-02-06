2d7e6e91758a35f1fa778399c96f665c916b056b
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2024-02-06 13:32:33 +0700

    added parsing

--- src/main/java/cft/test/task/Main.java
+++ src/main/java/cft/test/task/Main.java
@@ -1,5 +1,22 @@
 package cft.test.task;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Options options = parseOptions(args);
+        if (options == null) {
+            return;
+        }
+    }
+
+    private static Options parseOptions(String[] args) {
+        Options options;
+
+        try {
+            options = OptionsParser.parse(args);
+        } catch (UserInputException e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+        return options;
+    }
 }
