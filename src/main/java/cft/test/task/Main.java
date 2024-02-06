58b2c8a05e1e8487ba4dac21c0f4b1d30228a290
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2024-02-06 22:08:42 +0700

    added filtering

--- src/main/java/cft/test/task/Main.java
+++ src/main/java/cft/test/task/Main.java
@@ -1,11 +1,26 @@
 package cft.test.task;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Options options = parseOptions(args);
         if (options == null) {
             return;
         }
+
+        startUtilityWork(options);
+    }
+
+    private static void startUtilityWork(Options options) {
+        FilesFilter filesFilter = new FilesFilter(options);
+
+        List<String> integers = new ArrayList<>();
+        List<String> floats = new ArrayList<>();
+        List<String> strings = new ArrayList<>();
+
+        filesFilter.filter(integers, floats, strings);
     }
 
     private static Options parseOptions(String[] args) {
