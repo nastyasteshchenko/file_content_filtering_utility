3ad55be17d5b0c406ad6abdb5fa5651c2fb92f1f
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2024-02-06 14:13:57 +0700

    started writing files filter

---
+++ src/main/java/cft/test/task/FilesFilter.java
@@ -0,0 +1,27 @@
+package cft.test.task;
+
+import java.nio.file.Path;
+import java.util.List;
+
+class FilesFilter {
+    private static final String DEFAULT_INTS_OUTPUT = "integers.txt";
+    private static final String DEFAULT_FLOATS_OUTPUT = "floats.txt";
+    private static final String DEFAULT_STRINGS_OUTPUT = "strings.txt";
+    private final Path intsOutputPath;
+    private final Path floatsOutputPath;
+    private final Path stringsOutputPath;
+    private final Options options;
+
+    FilesFilter(Options options) {
+        this.options = options;
+        String resultPath = options.outputPath().toString();
+        intsOutputPath = Path.of(resultPath, options.namesPrefix() + DEFAULT_INTS_OUTPUT);
+        floatsOutputPath = Path.of(resultPath, options.namesPrefix() + DEFAULT_FLOATS_OUTPUT);
+        stringsOutputPath = Path.of(resultPath, options.namesPrefix() + DEFAULT_STRINGS_OUTPUT);
+    }
+
+    void filter() {
+        List<Path> inputFiles = options.inputFiles();
+        //for (inputFiles)
+    }
+}
