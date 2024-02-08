a204929cbb298b4531b86a2688ce926add14be4c
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2024-02-08 12:08:45 +0700

    edited

--- src/main/java/cft/test/task/StatisticsManager.java
+++ src/main/java/cft/test/task/StatisticsManager.java
@@ -3,64 +3,68 @@ package cft.test.task;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 class StatisticsManager {
-
-    private final List<BigInteger> integers;
-    private final List<BigDecimal> floats;
+     private final List<String> strIntegers;
+    private final List<String> strFloats;
     private final List<String> strings;
-    private final String minInts;
-    private final String maxInts;
-    private final String minFloats;
-    private final String maxFloats;
-    private final String intsSum;
-    private final String floatsSum;
-    private final String intsAvr;
-    private final String floatsAvr;
-
-//    Полная статистика для чисел
-//    дополнительно содержит минимальное и максимальное значения, сумма и среднее.
-//    Полная статистика для строк, помимо их количества, содержит также размер самой
-//    короткой строки и самой длинной.
 
     StatisticsManager(List<String> integers, List<String> floats, List<String> strings) {
-        this.integers = integers.stream().map(BigInteger::new).toList();
-        this.floats = floats.stream().map(BigDecimal::new).toList();
+        this.strIntegers= integers;
+        this.strFloats = floats;
         this.strings = strings;
-
-        Optional<BigInteger> opMaxInts = this.integers.stream().max(BigInteger::compareTo);
-        Optional<BigInteger> opMinInts = this.integers.stream().min(BigInteger::compareTo);
-        maxInts = opMaxInts.isPresent() ? opMaxInts.get().toString() : "no max";
-        minInts = opMinInts.isPresent() ? opMinInts.get().toString() : "no min";
-        BigInteger biIntsSum = this.integers.stream().reduce(BigInteger.ZERO, BigInteger::add);
-        intsSum = biIntsSum.toString();
-        intsAvr = biIntsSum.divide(BigInteger.valueOf(this.integers.size())).toString();
-
-        Optional<BigDecimal> opMaxFloats = this.floats.stream().max(BigDecimal::compareTo);
-        Optional<BigDecimal> opMinFloats = this.floats.stream().min(BigDecimal::compareTo);
-        maxFloats = opMaxFloats.isPresent() ? opMaxFloats.get().toString() : "no max";
-        minFloats = opMinFloats.isPresent() ? opMinFloats.get().toString() : "no min";
-        BigDecimal bdFloatsSum = this.floats.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
-        floatsSum = bdFloatsSum.toString();
-        floatsAvr = bdFloatsSum.divide(BigDecimal.valueOf(this.floats.size()), RoundingMode.UNNECESSARY).toString();
-
     }
 
-    void printFullStatistics() {
+    void countAndPrintFullStatistics() {
+        List<BigInteger> integers = strIntegers.stream().map(BigInteger::new).toList();
+        List<BigDecimal> floats = strFloats.stream().map(BigDecimal::new).toList();
+
+        Optional<BigInteger> opMaxInts = integers.stream().max(BigInteger::compareTo);
+        Optional<BigInteger> opMinInts = integers.stream().min(BigInteger::compareTo);
+
+        String maxInts = opMaxInts.isPresent() ? opMaxInts.get().toString() : "no max";
+        String minInts = opMinInts.isPresent() ? opMinInts.get().toString() : "no min";
+
+        BigInteger biIntsSum = integers.stream().reduce(BigInteger.ZERO, BigInteger::add);
+
+        String intsSum = biIntsSum.toString();
+        String intsAvr = integers.isEmpty() ? "no avr" : biIntsSum.divide(BigInteger.valueOf(integers.size())).toString();
+
+        Optional<BigDecimal> opMaxFloats = floats.stream().max(BigDecimal::compareTo);
+        Optional<BigDecimal> opMinFloats = floats.stream().min(BigDecimal::compareTo);
+
+        String maxFloats = opMaxFloats.isPresent() ? opMaxFloats.get().toString() : "no max";
+        String minFloats = opMinFloats.isPresent() ? opMinFloats.get().toString() : "no min";
+
+        BigDecimal bdFloatsSum = floats.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        String floatsSum = bdFloatsSum.toString();
+        String floatsAvr = floats.isEmpty() ? "no avr" : bdFloatsSum.divide(BigDecimal.valueOf(floats.size()), RoundingMode.UNNECESSARY).toString();
+
+        Optional<String> opMinStrLength = strings.stream().min(Comparator.comparingInt(String::length));
+        Optional<String> opMaxStrLength = strings.stream().max(Comparator.comparingInt(String::length));
+        String minStrLength = opMinStrLength.map(s -> String.valueOf(s.length())).orElse("no min");
+        String maxStrLength = opMaxStrLength.map(s -> String.valueOf(s.length())).orElse("no max");
+
         String fullIntegersStatistics = String.format("Integers:\nMin:\t%s\nMax:\t%s\nSum:\t%s\nAvr:\t%s\n",
                 minInts, maxInts, intsSum, intsAvr);
 
         String fullFloatsStatistics = String.format("Floats:\nMin:\t%s\nMax:\t%s\nSum:\t%s\nAvr:\t%s\n",
                 minFloats, maxFloats, floatsSum, floatsAvr);
 
-        System.out.println(fullIntegersStatistics + fullFloatsStatistics);
+        String fullStringsStatistics = String.format("Strings:\nAmount:\t%s\nMin length:\t%s\nMax length:\t%s\n",
+                strings.size(), minStrLength, maxStrLength);
+
+        System.out.println("-----------------------\nFull statistics:\n");
+        System.out.println(fullIntegersStatistics + '\n' + fullFloatsStatistics + '\n' + fullStringsStatistics);
     }
 
-    void printShortStatistics() {
-        String shortStatistics = String.format("Short statistics:\nAmount of integers:\t%d\nAmount of floats:\t%d\nAmount of strings:\t%d",
-                integers.size(), floats.size(), strings.size());
+    void countAndPrintShortStatistics() {
+        System.out.println("-----------------------\nShort statistics:\n");
+        String shortStatistics = String.format("Amount of integers:\t%d\nAmount of floats:\t%d\nAmount of strings:\t%d",
+                strIntegers.size(), strFloats.size(), strings.size());
 
         System.out.println(shortStatistics);
     }
